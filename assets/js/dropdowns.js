28819f5c19b2b59b25e04d5be099a483f721600c
A1Asriel <27139654+A1Asriel@users.noreply.github.com>
2024-05-11 01:41:24 +0300

    Доработка выпадающих меню

---
+++ assets/js/dropdowns.js
@@ -0,0 +1,32 @@
+function toggleDropdown(id) {
+    var dropdown = document.getElementById(id);
+    dropdown.classList.toggle("show");
+    dropdown.parentElement.classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+                openDropdown.parentElement.classList.remove("show");
+            }
+        }
+    } else {
+        var curId = event.target.nextElementSibling.id;
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.id == curId) {
+                continue;
+            }
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+                openDropdown.parentElement.classList.remove("show");
+            }
+        }
+    }
+}
